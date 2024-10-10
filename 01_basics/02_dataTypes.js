409519eeb854b5af292e52059435893820db3e9f
Nikk-art <nikhilbabli02@gmail.com>
2024-10-10 17:32:26 +0000

    container config done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+// "use strict" ;treats all js code as newer version
+// alert( 3 + 3) // we are using nodejs, not browser
+// console.log(
+//     3 +
+//     3
+// ) code should be redable and future proof
+name = "nikk";
+age = null
+
+// premitive
+// const number = 123;
+// const bigInt = 12232342344323423;
+// const boolean = true or false;
+// null = stanalone value 
+// undefine = value has not assigned
+// symbol - unique values 
+
+// non-premetive
+// object = {}
+// array = []
+console.log(typeof name);
+console.log(typeof age);
