6c60ff70bef238f95ae28437e2b78ced982b2cae
Nikk-art <nikhilbabli02@gmail.com>
2024-10-11 17:02:55 +0000

    dataConversion

---
+++ 01_basics/03_dataConversion.js
@@ -0,0 +1,82 @@
+
+let score = "33"
+//  console.log(score);
+//  console.log(typeof score);
+
+ let valueTOInt = Number(score);
+//  console.log(valueTOInt);
+//  console.log(typeof valueTOInt);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+
+// let isLoggedIn = "nikk"
+isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+ // 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let num = 123 ;
+
+let toString = String(num);
+// console.log(toString);
+// console.log(typeof toString);
+
+
+// ++++++++++++++++++++++++++++++++++++++++operation+++++++++++++++++++++++++++++
+
+let val = 2;
+let negval = -val;
+// console.log(val);
+// console.log(negval);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "nikk";
+let str2 = "bob";
+let str3 = str1 + " " + str2 ;
+console.log(str3);
+console.log(typeof str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(1 + 2 + "2");
+console.log("1" + 2 + 2);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
+
+
+
+
+
+
+
+
+
+
